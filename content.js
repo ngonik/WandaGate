1d19e8fafce9493bcaee889d95140ef4dcaf1105
Nicolas Gonik <nicogonik@gmail.com>
2025-01-21 21:53:33 -0300

    first commit

---
+++ content.js
@@ -0,0 +1,25 @@
+chrome.storage.sync.get("blockedNames", (data) => {
+    const blockedNames = data.blockedNames || [];
+    const elements = document.querySelectorAll("h1, h2, h3, p, span"); // Ajustar según el sitio web
+
+    const siteConfig = {
+        "www.infobae.com": "a",
+        "www.lanacion.com.ar": "a",
+        "www.clarin.com": "article"
+    };
+
+    if (siteConfig[url]) {
+        selectors = siteConfig[url];
+    }
+
+    elements.forEach(el => {
+        if (blockedNames.some(name => el.innerText.includes(name))) {
+            const parentLink = el.closest(selectors);
+            if (parentLink) {
+                parentLink.style.display = "none";
+            } else {
+                el.style.display = "none";
+            }
+        }
+    });
+});
